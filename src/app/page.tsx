3479468ef35f012d3c9c3fb3da324541bddaaf34
Tino Purmann <tinopurmann@gmail.com>
2024-11-12 18:31:38 -0800

    Uncommented triple dots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import Image from "next/image";
 
 export default function Home() {
-    const r = [1, 2, 3];
+    // const r = [1, 2, 3];
   return (
     <div className="h-[100vh] w-full bg-black items-center justify-center flex flex-col">
-      <div className="flex flex-row gap-6">
-          {r.map((i) => (
-              <div key={i} className="w-[30px] h-[30px] bg-white"></div>
-          ))}
-      </div>
+      {/*<div className="flex flex-row gap-6">*/}
+      {/*    {r.map((i) => (*/}
+      {/*        <div key={i} className="w-[30px] h-[30px] bg-white"></div>*/}
+      {/*    ))}*/}
+      {/*</div>*/}
         <div className="flex flex-row gap-6 absolute bottom-36">
             <a href="https://www.linkedin.com/in/tinopurmann/">
                 <Image priority={true} src="/linkedin.svg" alt="LinkedIn Profile" width={40} height={40}/>
