ca34599dddf37db48300d80cb541ec993205a547
Tino Purmann <tinopurmann@gmail.com>
2024-11-12 18:06:42 -0800

    Added Links changed rootlayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 
+import Image from "next/image";
+
 export default function Home() {
     const r = [1, 2, 3];
   return (
@@ -8,6 +10,14 @@ export default function Home() {
               <div key={i} className="w-[30px] h-[30px] bg-white"></div>
           ))}
       </div>
+        <div className="flex flex-row gap-6 absolute bottom-36">
+            <a href="https://www.linkedin.com/in/tinopurmann/">
+                <Image priority={true} src="/linkedin.svg" alt="LinkedIn Profile" width={40} height={40}/>
+            </a>
+            <a href="https://calendly.com/tino-p1q/30min">
+                <Image src="/calendly.svg" alt="Schedule with Calendly" width={40} height={40} className="bg-white"/>
+            </a>
+        </div>
     </div>
   );
 }
