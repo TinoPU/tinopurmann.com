50a7da356cc0ea08e3bb6a7342bc5210260a35cc
Tino Purmann <tinopurmann@gmail.com>
2024-11-18 23:18:57 -0800

    styling

--- src/components/ui/project_preview.tsx
+++ src/components/ui/project_preview.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function ProjectPreview({img_src, title, description}: {img_src: string, title: string, description: string}) {
     return (
         <div className="flex-shrink-0 max-w-36">
-            <Image className="object-cover h-36 w-36" src={img_src} alt={title} height={60} width={60}/>
-            <p className="text-gray-400 pt-1 font-bold">{title}</p>
+            <Image className="object-cover h-36 w-36 rounded-sm" src={img_src} alt={title} height={60} width={60}/>
+            <p className="text-gray-400 pt-1 font-bold text-xs">{title}</p>
             <p className="text-gray-400 text-xs">{description}</p>
         </div>
     );
