a9423f7371446839750633626fc42590c1eb8ae7
Tino Purmann <tinopurmann@gmail.com>
2024-11-20 11:46:15 -0800

    Added back button functionality

--- src/app/resources/tools/page.tsx
+++ src/app/resources/tools/page.tsx
@@ -1,19 +1,19 @@
 import ToolList from "@/components/ToolList";
-import Back from "@/components/icons/back";
 import Image from "next/image";
+import BackButton from "@/components/BackButton";
+
 
 
 
 
 export default function ToolPage() {
-
     return (
         <div className="flex flex-col">
             <div className="w-full fixed top-0 left-0 right-0 z-10 text-gray-400 p-3">
-                <Back />
+                <BackButton />
             </div>
             <div className="bg-gradient-to-b from-lime-200 bg-opacity-20 pb-6">
-                <div className="flex items-center justify-center px-16 pt-6 pb-3">
+            <div className="flex items-center justify-center px-16 pt-6 pb-3">
                     <Image className="w-full drop-shadow-2xl" src="/assets/resources/tools.png" alt="Tools" width={80} height={80}/>
                 </div>
                 <p className="text-gray-400 px-3 text-sm">
