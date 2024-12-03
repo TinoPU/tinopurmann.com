6e38eab764dcc3b378b5f2e04d7e9b9210a455d9
Tino Purmann <tinopurmann@gmail.com>
2024-12-02 16:28:37 -0800

    Updated Contact

--- src/components/ContactGrid.tsx
+++ src/components/ContactGrid.tsx
@@ -5,15 +5,20 @@ import VoiceRecorder from "@/components/VoiceRecorder";
 
 export default function ContactGrid() {
     return (
-        <div className="grid grid-cols-2 gap-3 pr-6">
-            <div className="flex flex-col gap-3">
-                <VoiceRecorder />
-                <QuickAccess image_src="/assets/contact/calendly.svg" alt_text="Calendly"
-                             link="https://calendly.com/tino-p1q/30min"/>
-            </div>
-            <div className="flex flex-col gap-3">
-                <QuickAccess image_src="/assets/contact/post.png" alt_text="Send me a Postcard" link="https://www.post.ch/en/sending-letters/sending-letters/postcard-creator-app#app-herunterladen"/>
-                <QuickAccess image_src="/assets/contact/mail.svg" alt_text="Please don't email me" link="mailto:tinopurmann@gmail.com"/>
+        <div className="flex flex-col gap-3">
+            <div className="text-white text-2xl font-bold">Contact</div>
+            <div className="grid grid-cols-2 gap-3 pr-6">
+                <div className="flex flex-col gap-3">
+                    <VoiceRecorder/>
+                    <QuickAccess image_src="/assets/contact/mail.svg" alt_text="Please don't email me"
+                                 link="mailto:tinopurmann@gmail.com"/>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <QuickAccess image_src="/assets/contact/post.png" alt_text="Send me a Postcard"
+                                 link="https://www.post.ch/en/sending-letters/sending-letters/postcard-creator-app#app-herunterladen"/>
+                    <QuickAccess image_src="/assets/contact/calendly.svg" alt_text="Calendly"
+                                 link="https://calendly.com/tino-p1q/30min"/>
+                </div>
             </div>
         </div>
     );
