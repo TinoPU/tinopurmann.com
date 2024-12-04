9544ffe9df6b1e82f10b10c5d06157726bb744fb
Tino Purmann <tinopurmann@gmail.com>
2024-12-03 23:25:34 -0800

    Desktop Responsiveness update

--- src/app/resources/reading/page.tsx
+++ src/app/resources/reading/page.tsx
@@ -8,19 +8,21 @@ import ReadingList from "@/components/ReadingList";
 
 export default function Reading() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
             <div className="w-full fixed top-0 left-0 right-0 z-10 text-gray-400 p-3">
                 <BackButton />
             </div>
-            <div className="bg-gradient-to-b from-[#5e6d86] bg-opacity-20 pb-6">
-                <div className="flex items-center justify-center px-16 pt-6 pb-3">
+            <div className="w-full bg-gradient-to-b from-[#5e6d86] bg-opacity-20 pb-6 md:flex md:flex-col md:items-center">
+                <div className="flex items-center justify-center px-16 pt-6 pb-3 md:px-3 md:w-1/3">
                     <Image className="w-full drop-shadow-2xl" src="/assets/resources/reading.jpg" alt="Newsletters" width={80} height={80} unoptimized={true}/>
                 </div>
                 <p className="text-gray-400 px-3 text-sm">
                     Current reading list. I need suggestions.
                 </p>
             </div>
-            <ReadingList />
+            <div className="w-full md:w-1/3">
+                <ReadingList />
+            </div>
         </div>
     )
 }
